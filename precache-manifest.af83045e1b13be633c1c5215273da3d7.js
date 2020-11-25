da54c973d4af7fd755dda059dd47417cb8eccbbe
Bhavyajot Malhotra <malhotrabhavyajot@gmail.com>
2020-11-25 23:17:07 +0530

    Updates

---
+++ precache-manifest.af83045e1b13be633c1c5215273da3d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c726cc238a2996d3d3d551d8f4bac0",
+    "url": "/ToDo_App/index.html"
+  },
+  {
+    "revision": "61f2d9c13263fae7f4e2",
+    "url": "/ToDo_App/static/css/main.fed6920a.chunk.css"
+  },
+  {
+    "revision": "6ecf24a68dcef1ac70a7",
+    "url": "/ToDo_App/static/js/2.2af9e1f7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ToDo_App/static/js/2.2af9e1f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61f2d9c13263fae7f4e2",
+    "url": "/ToDo_App/static/js/main.8b3eee51.chunk.js"
+  },
+  {
+    "revision": "acec2131b0d22434a458",
+    "url": "/ToDo_App/static/js/runtime-main.1b6963c1.js"
+  }
+]);
